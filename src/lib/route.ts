46a4565e644da8c2afcde49eba41163b42e3e232
Kai Shao <grepug@163.com>
2018-01-11 11:48:47 +0800

    ..

--- src/lib/route.ts
+++ src/lib/route.ts
@@ -1,4 +1,5 @@
 import { wx } from './wx'
+import { store } from './store'
 
 export class Route {
 
@@ -10,8 +11,10 @@ export class Route {
 
     }
 
-    navigateTo() {
-        wx.navigateTo()
+    async navigateTo(path, requestFunc: Promise<any>) {
+        wx.navigateTo({ url: path })
+        let res = await requestFunc
+        store.mutate({ requestData: { path: res } })
     }
 
     navigateBack() {
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,23 @@
+import * as _ from 'lodash-es'
+
 export class Store {
 
-    
+    private state: any = {
+        currentPath: '',
+        requestData: {
 
+        }
+    }
+    
+    get getters() {
+        return 
+    }
+
+    mutate(obj: any) {
+        _.extend(this.state, obj)
+    }
 
 
 }
+
+export const store = new Store
