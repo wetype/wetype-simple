e6bc01a435db3a1a9b0367b44a040490550f01c9
Kai Shao <grepug@163.com>
2018-01-16 11:03:41 +0800

    恢复 rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,19 @@
-null
+import typescript from 'rollup-plugin-typescript2'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+// import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/wetype.js',
+        format: 'cjs'
+    },
+    plugins: [
+        typescript({
+            tsconfig: 'tsconfig.rollup.json'
+        }),
+        resolve(),
+        babel()
+    ]
+}
